4af9edfbc8493b514025a2e9162fc23290edf28d
wesselrijk <wesselrijk@gmail.com>
2019-05-20 21:12:37 +0200

    Latest MAC

--- app/src/main/java/com/example/trivia/Game.java
+++ app/src/main/java/com/example/trivia/Game.java
@@ -5,13 +5,43 @@ import java.io.Serializable;
 public class Game implements Serializable {
 
     private int questionsNumber;
-    private String category;
+    private int category;
     private String difficulty;
     private String type;
     private String encoding;
     private Boolean gameOver;
 
     public Game() {
+        this.questionsNumber = 10;
+        this.category = 8;
+        this.difficulty = null;
+        this.type = null;
+        this.encoding = null;
+    }
 
+    // list of getters
+    public int getQuestionsNumber() { return this.questionsNumber; }
+    public int getCategory() {return this.category; }
+    public String getDifficulty() {return this.difficulty; }
+    public String getType() {return this.type; }
+    public String getEncoding() {return this.encoding; }
+    public Boolean getGameOver() {return this.gameOver; }
+
+    // list of setters
+    public void setQuestionsNumber(int questionsNumber) {
+        this.questionsNumber = questionsNumber;
+    }
+    public void setCategory(int category) { this.category = category; }
+    public void setDifficulty(String difficulty) {
+        this.difficulty = difficulty;
+    }
+    public void setType(String type) {
+        this.type = type;
+    }
+    public void setEncoding(String encoding) {
+        this.encoding = encoding;
+    }
+    public void setGameOver(Boolean gameOver) {
+        this.gameOver = gameOver;
     }
 }
--- app/src/main/java/com/example/trivia/MainActivity.java
+++ app/src/main/java/com/example/trivia/MainActivity.java
@@ -2,26 +2,30 @@ package com.example.trivia;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
 
 import java.util.ArrayList;
 import java.util.List;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
 
+    Game game;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        game  = new Game();
 
         List<Integer> spinnerArray = new ArrayList<>();
         for (int i = 1; i <= 50; i++) {
             spinnerArray.add(i);
         }
 
-        /* set first spinner using:
+        /* set all spinners using:
         * https://developer.android.com/guide/topics/ui/controls/spinner#java */
         Spinner spinnerQuestionNumber = findViewById(R.id.spinnerQuestionsNumber);
         ArrayAdapter<Integer> adapterNumbers = new ArrayAdapter<Integer>(
@@ -31,6 +35,8 @@ public class MainActivity extends AppCompatActivity {
         );
         adapterNumbers.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         spinnerQuestionNumber.setAdapter(adapterNumbers);
+        spinnerQuestionNumber.setOnItemSelectedListener(this);
+        spinnerQuestionNumber.setSelection(9);
 
 
         Spinner spinnerCategory = findViewById(R.id.spinnerCategory);
@@ -38,6 +44,7 @@ public class MainActivity extends AppCompatActivity {
                 R.array.category_array, android.R.layout.simple_spinner_item);
         adapterCategories.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         spinnerCategory.setAdapter(adapterCategories);
+        spinnerCategory.setOnItemSelectedListener(this);
 
 
         Spinner spinnerDifficulty = findViewById(R.id.spinnerDifficulty);
@@ -45,6 +52,7 @@ public class MainActivity extends AppCompatActivity {
                 R.array.difficulty_array, android.R.layout.simple_spinner_item);
         adapterDifficulty.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         spinnerDifficulty.setAdapter(adapterDifficulty);
+        spinnerDifficulty.setOnItemSelectedListener(this);
 
 
         Spinner spinnerType = findViewById(R.id.spinnerType);
@@ -52,8 +60,44 @@ public class MainActivity extends AppCompatActivity {
                 R.array.type_array, android.R.layout.simple_spinner_item);
         adapterType.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         spinnerType.setAdapter(adapterType);
+        spinnerType.setOnItemSelectedListener(this);
     }
 
+    public void onItemSelected(AdapterView<?> parent, View view,
+                               int pos, long id) {
+        // An item was selected. You can retrieve the selected item using
+        switch (parent.getId()) {
+            case R.id.spinnerQuestionsNumber:
+                game.setQuestionsNumber(pos - 1);
+                Log.d("selected", String.valueOf(game.getQuestionsNumber()));
+            case R.id.spinnerCategory:
+                game.setCategory(pos + 8);
+                Log.d("selected", String.valueOf(game.getCategory()));
+            case R.id.spinnerDifficulty:
+                String setDifficulty = parent.getItemAtPosition(pos).toString();
+                if (setDifficulty == "Easy") {
+                    game.setDifficulty("easy");
+                } else if (setDifficulty == "Medium") {
+                    game.setDifficulty("medium");
+                } else if (setDifficulty == "Hard") {
+                    game.setDifficulty("hard");
+                }
+                Log.d("selected", game.getDifficulty());
+            case R.id.spinnerType:
+                String setType = parent.getItemAtPosition(pos).toString();
+                if (setType == "Multiple Choice") {
+                    game.setType("multiple");
+                } else if (setType == "True / False") {
+                    game.setType("boolean");
+                }
+                Log.d("selected", game.getType());
+        }
+
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {
+    }
 
     public void playClicked(View view) {
 
